131e63cdba472b3e44eeb0dda0e75b97411abae5
imen-lakrib <lak12imen@gmail.com>
2023-07-22 16:46:28 +0100

    next app

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+     <h1>CMS</h1>
+
+      
+
+      
+    </main>
+  )
+}
